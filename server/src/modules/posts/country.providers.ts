79428121d2e454cb6d62cdb954b28ae0aed5f04e
yardenbd <84677692+yardenbd@users.noreply.github.com>
2023-01-16 13:36:20 +0200

    Delete server/src/modules/posts directory

--- server/src/modules/posts/country.providers.ts
+++
@@ -1,9 +0,0 @@
-import { Country } from './country.entity';
-import { COUNTRY_REPOSITORY } from '../../core/constants';
-
-export const postsProviders = [
-  {
-    provide: COUNTRY_REPOSITORY,
-    useValue: Country,
-  },
-];
--- server/src/modules/posts/dto/update-country.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UpdateCountryDto {
-  id: number;
-  name?: string;
-  code?: string;
-  flag?: string;
-  longitude?: number;
-  latitude?: number;
-  description?: string;
-}
